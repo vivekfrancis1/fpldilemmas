c1f868fdd1013e9eb3d6a850d0d88fbda08d3fb0
vivekfrancis <46624072-vivekfrancis@users.noreply.replit.com>
2025-08-27 10:39:50 +0000

    Restored to '9738d3b5d9883ff5284934c554087efd3d7ee520'
    Replit-Restored-To: 9738d3b5d9883ff5284934c554087efd3d7ee520

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,8 @@ import AssistShare from "./pages/assist-share";
 import PlayerGoalProjections from "./pages/player-goal-projections";
 import PlayerAssistProjections from "./pages/player-assist-projections";
 import Watchlist from "./pages/watchlist";
-import MyFPLDashboard from "./pages/my-fpl-dashboard";
+import LiveRank from "./pages/live-rank";
+import MyTeam from "./pages/my-team";
 import MyLeagues from "./pages/my-leagues";
 import PriceTracker from "./pages/price-tracker";
 import RecentPriceChanges from "./pages/recent-price-changes";
@@ -46,12 +47,13 @@ function Router() {
   
   return (
     <Switch>
-      <Route path="/" component={MyFPLDashboard} />
-      <Route path="/my-dashboard" component={MyFPLDashboard} />
+      <Route path="/" component={LiveRank} />
+      <Route path="/live-rank" component={LiveRank} />
       <Route path="/fixtures" component={Fixtures} />
       <Route path="/transfers" component={Transfers} />
       <Route path="/captain" component={Captain} />
       <Route path="/watchlist" component={Watchlist} />
+      <Route path="/my-team" component={MyTeam} />
       <Route path="/my-leagues" component={MyLeagues} />
       <Route path="/price-tracker" component={PriceTracker} />
       <Route path="/recent-price-changes" component={RecentPriceChanges} />
